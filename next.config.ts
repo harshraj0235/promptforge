06253229a868dbab603486069702cba6cf82dbc3
Harsh Raj <harshraj@newgensoft.com>
2026-06-17 11:47:04 +0530

    fix: change Next.js output to standalone to fix Cloudflare opennextjs build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   // Optional: Change image optimization if you're using next/image, 
   // since standard Next.js Image Optimization doesn't work in static exports.
   images: {
